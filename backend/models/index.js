61561706f7a9ec71be0d1805b070391829fc2ff4
wiissal <wissaloa1@gmail.com>
2025-12-05 11:11:29 +0100

    create sequelize instance

--- backend/models/index.js
+++ backend/models/index.js
@@ -6,3 +6,10 @@ const env = process.env.NODE_ENV || 'development'; // see what env  if its dev o
 const config = require(path.join(__dirname, '..','config','config.json'))[env]; 
 const db={}; //it gonna hold all models
 
+//sequelize instance
+let sequelize;
+if(config.use_env_variable){
+  sequelize = new Sequelize(process.env[config.use_env_variable], config); // if werw using db url env variable
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);//if we use config,json 
+}
