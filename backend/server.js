a1ef3b3294662b7599f25d8bdaa168d6238d73fe
wiissal <wissaloa1@gmail.com>
2025-12-06 13:20:44 +0100

    server.js/ create main express app and process requests

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const db = require('./models');
+const app = express();
+
+app.use(cors())  //connect the backend with front end req
+app.use(express.json()); //access to data and let express read json req coming
