5c84d4c81743b53d9e5c0f7dfd1daba31135d6ea
wiissal <wissaloa1@gmail.com>
2025-12-06 23:12:34 +0100

    fix routes/task

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const taskController = require("../controllers/tasksController");
+const taskController = require("../controllers/taskController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.get("/", authMiddleware, taskController.getTasks);
