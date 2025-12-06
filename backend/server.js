d5f4223b18be2476689b628d0ea1222ff38ea5e0
wiissal <wissaloa1@gmail.com>
2025-12-06 13:39:25 +0100

    adding auth and task routes

--- backend/server.js
+++ backend/server.js
@@ -7,3 +7,8 @@ const app = express();
 
 app.use(cors())  //connect the backend with front end req
 app.use(express.json()); //access to data and let express read json req coming
+
+//auth/task routes
+app.use('/api/auth', require('./routes/auth'));
+
+
