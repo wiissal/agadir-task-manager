0cab8fdb38e90b95881e7eb1e269535f7e9c406b
wiissal <wissaloa1@gmail.com>
2025-12-06 13:45:13 +0100

    add error handling and start the server

--- backend/server.js
+++ backend/server.js
@@ -10,5 +10,19 @@ app.use(express.json()); //access to data and let express read json req coming
 
 //auth/task routes
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/tasks', require('./routes/task'));
 
+app.use((err, req, res, next) => {
+console.error(err.stack);
+res.status(500).json({message: 'server error', error: err.message});
+});
 
+const PORT = process.env.PORT || 5000;
+db.sequelize.sync().then(()=>{
+  app.listen(PORT,()=>{
+    console.log(`Server is running on  http://localhost:${PORT}`);
+  });
+}).catch((err)=>{
+  console.log('Data base sync error:' , err);
+});
+module.exports = app;
