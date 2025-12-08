834404b37fe62a37290e9ba588f77ee40d8d0630
wiissal <wissaloa1@gmail.com>
2025-12-08 13:43:09 +0100

    set up stack navigation structure

---
+++ frontend/src/navigation/appNavigator.js
@@ -0,0 +1,39 @@
+import react from 'react';
+import {NaviagtionContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import SplashScreen from '../screens/SplashScreen';
+import LoginScreen from 'screens/LoginScreen';
+import RegisterScreen from 'screens/RegisterScreen';
+import TaskListScreen from 'screens/TaskListScreen';
+import AddTaskScreen from 'screens/AddTaskScreen';
+import TaskHistoryScreen from 'screens/TaskHistoryScreen';
+
+const stack = createStackNavigator();
+export const AppNavigator = ({isLoggedIn}) =>{
+  return ( 
+<NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        {isSplashLoading ? (
+          <Stack.Screen name="Splash" component={SplashScreen} />
+        ) : !isLoggedIn ? (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="TaskList" component={TaskListScreen} />
+            <Stack.Screen name="AddTask" component={AddTaskScreen} />
+            <Stack.Screen name="TaskHistory" component={TaskHistoryScreen} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+  
